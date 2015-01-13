cf29ef596db90841076e8c2e1961a65b23e19f11
Chris Teung <cteung@uw.edu>
2015-01-13 02:44:32 -0800

    no message

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,7 +3,7 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person>{
   private int age;
   private String name;
   private double salary;
@@ -20,21 +20,53 @@ public class Person {
     salary = s;
   }
 
+  public int compare(Person p1, Person p2){
+    double d = p2.getSalary()- p1.getSalary();
+    return (int) d;
+  }
+
+  public int compareTo(Person other){
+    return other.age - this.age;
+  }
+
+  public void setSalary(double salary){
+    this.salary = salary;
+  }
+
+  public void setAge(int age) {
+    if (age < 0){
+      throw new IllegalArgumentException("invalid age");
+    }
+    this.age = age;
+  }
+
+  public void setName(String name) {
+    if (name == null){
+      throw new IllegalArgumentException("name is null");
+    }
+    this.name = name;
+  }
+
   public int getAge() {
+
     return age;
   }
-  
+
   public String getName() {
+
     return name;
   }
-  
+
   public double getSalary() {
+
     return salary;
   }
   
   public String getSSN() {
+
     return ssn;
   }
+
   public void setSSN(String value) {
     String old = ssn;
     ssn = value;
@@ -42,6 +74,7 @@ public class Person {
     this.pcs.firePropertyChange("ssn", old, value);
     propertyChangeFired = true;
   }
+
   public boolean getPropertyChangeFired() {
     return propertyChangeFired;
   }
@@ -59,11 +92,11 @@ public class Person {
   }
   
   public boolean equals(Person other) {
-    return (this.name.equals(p.name) && this.age == p.age);
+    return ((this.getName()).equals(other.getName()) && (this.getAge())==(other.getAge()));
   }
 
   public String tostring() {
-    return "{{FIXME}}";
+    return "[Person name:"+this.getName()+" age:"+String.valueOf(this.getAge())+" salary:"+String.valueOf(this.getSalary())+"]";
   }
 
   // PropertyChangeListener support; you shouldn't need to change any of
@@ -75,5 +108,21 @@ public class Person {
   }
   public void removePropertyChangeListener(PropertyChangeListener listener) {
       this.pcs.removePropertyChangeListener(listener);
+  }
+
+  public static ArrayList<Person> getNewardFamily() {
+    ArrayList<Person> family = new ArrayList<Person>();
+    family.add(new Person("Ted", 41, 250000));
+    family.add(new Person("Charlotte", 43, 150000));
+    family.add(new Person("Michael", 22, 10000));
+    family.add(new Person("Matthew", 15, 0));
+    return family;
+  }
+
+  public static class AgeComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2){
+      return p1.compareTo(p2);
+    }
   }
 }
